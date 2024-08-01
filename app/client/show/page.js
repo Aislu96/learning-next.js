4392ccc09751164c852e53505b61e04dff82cf5f
aislusha96 <arslanova1996ais@list.ru>
2024-08-01 11:00:43 +0300

    "Получение и обработка данных"

--- app/client/show/page.js
+++
@@ -1,6 +0,0 @@
-
-export default function Show() {
-    return (
-        <h1>Show страница</h1>
-    );
-}
--- app/page.js
+++ app/page.js
@@ -1,5 +1,22 @@
-export default function Home() {
-  return (
-  <h1>Главная страница</h1>
-  );
+import Link from "next/link";
+
+async function fetchData() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+    return await res.json();
+}
+
+export default async function Home() {
+    const posts = await fetchData();
+    return (
+        <div className="main">
+            <h1>Главная страница</h1>
+            {posts.map(el=> (
+                <div key={el.id} className="post">
+                    <h2>{el.title}</h2>
+                    <p>{el.body}</p>
+                    <Link href={`/post/` + el.id}>детальнее</Link>
+                </div>
+            ))}
+        </div>
+    );
 }
